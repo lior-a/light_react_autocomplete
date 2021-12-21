08b666c0ff29c3d1ac1dd17654538d7dd968c70a
lior-a <lior.tauill@gmail.com>
2021-12-21 16:32:48 +0200

    small pr bugfix

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,2 @@
-console.log('[DEVELOPER]: Loading setupTests.js')
 import regeneratorRuntime from "regenerator-runtime";
 import '@testing-library/jest-dom';
-// import 'react';
-import * as React from 'react';
